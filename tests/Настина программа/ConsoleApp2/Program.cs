d9c0e83d8f21adef47ef6664b6731e5975ea4c76
Nadezda1998 <konovalova-nadya@list.ru>
2019-05-19 21:13:20 +0500

    add tests

---
+++ tests/Настина программа/ConsoleApp2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class MinMaxLen
+    {
+        public static void Main(string[] args)
+        {
+            /*1*/
+            Console.Write("Введите строку: ");          //вывод сообщения                                                    
+                                                        /*2*/
+            string input = Console.ReadLine();          //ввод с клавиатуры                                                  
+                                                        /*3*/
+            string[] str;
+            int max, min;
+            Proverka(input, out str, out max, out min);//конец цикла
+
+            /*13*/
+            Console.Write("Самое длинное слово: {0}, самое короткое слово: {1} ", str[max], str[min]);      //вывод результата
+            Console.ReadLine();
+        }
+
+        public static void Proverka(string input, out string[] str, out int max, out int min)
+        {
+            str = input.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            /*4*/
+            int maxlen = int.MinValue, minlen = int.MaxValue;     //ввод параметров
+            max = 0;
+            min = 0;
+
+            /*5*/
+            for (int i = 0; i < str.Length; i++)         //начало цикла, условие i < str.Length
+            {
+
+                /*6*/
+                MaxLen(str, ref maxlen, ref max, i);     //условие str[i].Length > maxlen
+
+                /*7*/
+            }                                           //конец цикла
+
+            /*8*/
+            for (int i = 0; i < str.Length; i++)   //начало цикла, условие i < str.Length
+            {
+
+                /*9*/
+                if (str[i].Length < minlen)        //условие str[i].Length < minlen
+                {
+
+                    /*10*/
+                    minlen = str[i].Length;             //условие minlen = str[i].Length
+
+                    /*11*/
+                    min = i;                            //условие min = i
+                }
+
+                /*12*/
+            }
+        }
+
+        public static void MaxLen(string[] str, ref int maxlen, ref int max, int i)     
+        {
+            
+       if (str[i].Length > maxlen)        
+            {
+               
+          maxlen = str[i].Length;     
+                max = i;
+                
+            }      
+        }          
+    }
+
+    
+     
+
+}
---
+++ tests/Настина программа/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ConsoleApp2;
+
+namespace ConsoleApp2
+{
+    /*Структурный тест*/
+    /*1*/
+    [TestClass]
+    public class MinMaxLenTest
+    {
+        [TestMethod]
+        public void MinTest()
+        {
+            string input = "Люблю грозу в начале мая";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(2, min);
+            Assert.AreEqual(3, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { "Люблю", "грозу", "в", "начале", "мая" }));
+        }
+
+        /*2 структурный и функциональный тест, граничное значение*/
+        [TestMethod]
+        public void MinTest2()
+        {
+            string input = "";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(0, min);
+            Assert.AreEqual(0, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { }));
+        }
+
+        /*Структурный тест*/
+        /*3*/
+        [TestMethod]
+        public void MinTest3()
+        {
+            string input = "!";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(0, min);
+            Assert.AreEqual(0, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { }));
+        }
+
+        /*Структурный тест*/
+        /*4*/
+        [TestMethod]
+        public void MinTest4()
+        {
+            string input = "Seattle is a big city";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(2, min);
+            Assert.AreEqual(0, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { "Seattle", "is", "a", "big", "city" }));
+        }
+
+        /*2 и структурный, и функциональный тест*/
+        /*5*/
+        [TestMethod]
+        public void MinTest5()
+        {
+            string input = "В городе было тепло";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(0, min);
+            Assert.AreEqual(1, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { "В", "городе", "было", "тепло" }));
+        }
+
+        /*ФункциональнЫЙ тест*/
+        /*6*/
+        [TestMethod]
+        public void MinTest6()
+        {
+            string input = "Из истории вычислительной техники";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(0, min);
+            Assert.AreEqual(2, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { "Из", "истории", "вычислительной", "техники" }));
+        }
+
+        /*ФункциональнЫЙ тест*/
+        /*7*/
+        [TestMethod]
+        public void MinTest7()
+        {
+            string input = "Сегодня холодно";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(0, min);
+            Assert.AreEqual(0, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { "Сегодня", "холодно" }));
+        }
+
+        /*ФункциональнЫЙ тест и граничное значение*/
+        /*8*/
+        [TestMethod]
+        public void MinTest8()
+        {
+            string input = "Я";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+
+            Assert.AreEqual(0, min);
+            Assert.AreEqual(0, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { "Я" }));
+        }
+
+
+        /*ФункциональнЫЙ тест*/
+        /*9*/
+        [TestMethod]
+        public void MinTest9()
+        {
+            string input = "Тысячадевятьсотвосьмидесятидевятимиллиметровый";
+            string[] str;
+            int max, min;
+
+            MinMaxLen.Proverka(input, out str, out max, out min);
+        
+            Assert.AreEqual(0, min);
+            Assert.AreEqual(0, max);
+            Assert.IsTrue(str.SequenceEqual(new String[] { "Тысячадевятьсотвосьмидесятидевятимиллиметровый" }));
+        }
+    }
+}
